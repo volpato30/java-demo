4526181f6abc0128b78bf98dc8210ea31e198425
rqiao <volpato30@gmail.com>
2020-01-14 12:41:52 -0500

    test

--- README.md
+++ README.md
@@ -9,16 +9,11 @@ From the pytorch.org homepage under "Quick Start Locally",
 make sure "LibTorch" is the selected package.
 As of this writing, only Linux is supported.  Mac and Windows will follow.
 
-Run `export LIBTORCH_HOME=/path/to/libtorch`.
-The `build.gradle` file will use this to set `java.library.path`
-when running the application.
-If you are using PyTorch in your own environment,
-`LIBTORCH_HOME` is not necessary.
-Instead, you will need to set `java.library.path` to `/path/to/libtorch/lib`.
+
+Change `java.library.path` in `build.gradle` to `/path/to/libtorch/lib`.
 
 Run `./gradlew run` to build and run the demo application.
 It will load `demo-model.pt` and run it on some simple data.
-[This notebook](TorchScriptForJavaDemo.ipynb) was used to generate the model.
 
 More information about the Java API and TorchScript:
 - [TorchScript tutorial](https://pytorch.org/tutorials/beginner/Intro_to_TorchScript_tutorial.html)
--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -7,19 +7,39 @@ import org.pytorch.Tensor;
 import java.util.Arrays;
 
 public class App {
+  private static float[] ones(int length) {
+    float[] result = new float[length];
+    for (int i = 0; i < length; i++) {
+      result[i] = 1.0f;
+    }
+    return result;
+  }
+
   public static void main(String[] args) {
-    Module mod = Module.load("demo-model.pt1");
-    Tensor data =
+    Module mod = Module.load("train/dist/forward_scripted.pt");
+    int vocab_size = 26;
+    int num_ion = 12;
+    int num_max_peaks = 500;
+    Tensor mz_of_interest =
         Tensor.fromBlob(
-            new int[] {1, 2, 3, 4, 5, 6}, // data
-            new long[] {2, 3} // shape
+            ones(vocab_size * num_ion), // data
+            new long[] {1, 1, vocab_size, num_ion} // shape
             );
-    IValue result = mod.forward(IValue.from(data), IValue.from(3.0));
+    Tensor spectrum_mz =
+            Tensor.fromBlob(
+                    ones(num_max_peaks), // data
+                    new long[] {1, num_max_peaks} // shape
+            );
+    Tensor spectrum_intensity =
+            Tensor.fromBlob(
+                    ones(num_max_peaks), // data
+                    new long[] {1, num_max_peaks} // shape
+            );
+
+    IValue result = mod.forward(IValue.from(mz_of_interest), IValue.from(spectrum_mz), IValue.from(spectrum_intensity));
     Tensor output = result.toTensor();
     System.out.println("shape: " + Arrays.toString(output.shape()));
     System.out.println("data: " + Arrays.toString(output.getDataAsFloatArray()));
-
-    // Workaround for https://github.com/facebookincubator/fbjni/issues/25
     System.exit(0);
   }
 }
